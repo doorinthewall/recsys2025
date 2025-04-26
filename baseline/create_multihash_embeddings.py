81f6da03dc06c5511ae7fa336dee9c3e3fcbc6c6
Aleksei Medvedev <a00931364@china.huawei.com>
2025-04-26 14:58:17 +0100

    add multihash

---
+++ baseline/create_multihash_embeddings.py
@@ -0,0 +1,23 @@
+import cityhash
+from baseline.aggregated_features_baseline import create_embeddings
+import torch
+
+def get_bin(ids, seed=42, n_bins=512):
+  transform = lambda x: cityhash.CityHash64(f"p/{seed}/{x}") % n_bins
+  return torch.tensor(list(map(transform, ids)), dtype=int)
+
+def one_hot_embed(bins, n_bins=512):
+  embeds = torch.zeros((bins.size(0), n_bins))
+  embeds.scatter_add_(1, bins.unsqueeze(1), torch.ones(bins.size(0), 1))
+  return embeds
+
+def create_multihash_embeddings(ids, seeds=[42, 666, 777, 4], n_bins=10):
+  bins = [get_bin(ids, seed, n_bins) for seed in seeds]
+  result = []
+  for bin in bins:
+    result += [one_hot_embed(bin, n_bins)] 
+  return torch.cat(result)
+
+if __name__ == "__main__":
+    relevant_ids = create_embeddings.load_relevant_clients_ids(Path("/home/recsys_dataset/unpacked_dataset/input"))
+    create_multihash_embeddings(relevant_ids)
